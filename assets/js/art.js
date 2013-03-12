ec1fd37e9cc5e8755d9434c3fb8601677196084b
John West <john@MMABANTA537431.local>
2013-03-12 17:48:47 -0400

    Adds a readme and minor additions (still not complete) to the art.js file.

--- assets/js/art.js
+++ assets/js/art.js
@@ -2,5 +2,8 @@ window.Art = Backbone.Model.extend({
   url: function() { return 'art?id=' + this.id; }
 });
 window.ArtView = Backbone.View.extend({
-  
+  class: 'art-wrapper',
+  initialize: function() { _.bindAll( this, 'render' ); },
+  render: function() {
+  }
 });
